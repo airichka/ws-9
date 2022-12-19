eb1d8282b9d431d9e93a3060dfe81a4378f9af63
Ira <airichka@mail.ru>
2022-12-19 17:32:46 +0400

    task68

--- Program.cs
+++ Program.cs
@@ -1,28 +1,28 @@
-﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
 
-N = 5 -> "5, 4, 3, 2, 1"
-N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+Console.Write("Введите число m: ");
 
-Console.Write("Введите число: ");
-
-if(uint.TryParse(Console.ReadLine(), out uint result))
+if(uint.TryParse(Console.ReadLine(), out uint resultM))
 {
- Console.Write($"N = {result} -> ");
- WriteNums(result);
+    Console.Write("Введите число n: ");
+    if(uint.TryParse(Console.ReadLine(), out uint resultN))
+    {
+        Console.Write($"m = {resultM}; n = {resultN} -> A(m, n) = {FuncAkkerman(resultM, resultN)}");
+    }
 }
 else
 {
- Console.WriteLine("Вы ввели не натуральное число");
+    Console.WriteLine("Вы ввели не натуральное число");
 }
 
-uint WriteNums(uint n)
+static uint FuncAkkerman(uint m, uint n)
 {
- if (n > 1)
- {
- Console.Write($"{n}, ");
- return WriteNums(--n);
- }
-
- Console.Write(n);
- return n;
+    if (m == 0)
+        return n + 1;
+    else if ((m != 0) && (n == 0))
+        return FuncAkkerman(m - 1, 1);
+    else
+        return FuncAkkerman(m - 1, FuncAkkerman(m, n - 1));
 }
